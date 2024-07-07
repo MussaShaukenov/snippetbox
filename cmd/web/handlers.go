cb76367a8bf745f7eb642d180b06e43d50a2c0d4
Mussa <mshaukenov@gmail.com>
2024-07-07 23:28:03 +0500

    revert: chapter 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+<<<<<<< HEAD
 	"errors"
 	"fmt"
 	"github.com/MussaShaukenov/snippetbox/internal/models"
+=======
+	"fmt"
+	"html/template"
+>>>>>>> parent of ab1feef (chapter 5: database-driven responses)
 	"net/http"
 	"strconv"
 )
@@ -14,6 +19,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+<<<<<<< HEAD
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -26,6 +32,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data.Snippets = snippets
 
 	app.render(w, http.StatusOK, "home.tmpl", data)
+=======
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file
+	// paths as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // use serverError() helper
+		return
+	}
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err) // use serverError() helper
+	}
+>>>>>>> parent of ab1feef (chapter 5: database-driven responses)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +65,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+<<<<<<< HEAD
 
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
@@ -49,6 +81,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
+=======
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+>>>>>>> parent of ab1feef (chapter 5: database-driven responses)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -57,19 +93,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
 
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	// Redirect the user to the relevant page for the snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	w.Write([]byte("Create a new snippet..."))
 }
